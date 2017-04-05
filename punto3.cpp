d53f70fed32bce445ef0d51fb2277bd8ecb639bf
Julian Ruiz <julian.david.ruiz@correounivalle.edu.co>
2017-04-05 11:39:55 -0500

    se realizo el primer el punto 3 del parcial

---
+++ punto3.cpp
@@ -0,0 +1,37 @@
+/*
+ * realizado por: Julian Rico Millan, Diego F Gonzales Rojas, Julian Ruiz
+ * archivo: parcialEjercicio5.cpp parcial A
+ * descripcion: funcion  que reciba  dos numeros  enteros, y retorna un char.
+ * fecha: 5 de abril 2017
+*/
+
+#include <iostream>
+using namespace std;
+
+int a;
+int b;
+
+int main (){
+	
+	cout << "ingrese a" <<endl;
+	cin >> a;
+	cout << "ingrese b" <<endl;
+	cin >> b; 
+
+	if( b > 0 && a > 0){		
+		cout <<"c" <<endl;}
+		
+		else if( b < 0){
+			
+			 if(a > 5 or b < -5){
+				cout <<"d" <<endl; }
+				
+				else if( a <= 5 and b > 5){
+					cout <<"f" << endl;}
+					
+					else cout <<"k"<<endl;}
+					
+						else if(((b = 0 )and a > 0)){
+							cout<<"u"<<endl; }
+							
+								else cout<<"v"<<endl; }
